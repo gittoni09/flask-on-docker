27dbfa83587cbd8e6a1b05bb1008473c854c4914
Royo Moraga, Antonio <antonio.royo.moraga@accenture.com>
2017-11-13 10:36:17 +0100

    Initial commit

---
+++ hello.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+import os
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+#Print host name for debugging purposes
+hostname = os.environ['COMPUTERNAME']
+print "Hostname: ", hostname
+
+@app.route('/')
+def index():
+    return render_template('index.html', hostname=hostname )
+
+@app.route('/c2f')
+def c2f():
+    celsiusList = []
+    farenList = []
+    for cel in range(-20,101,10):
+        celsiusList.append(cel) 
+        faren = ((cel/5)*9)+32
+        farenList.append(faren)
+        cel =+ 10
+    zipped = zip(celsiusList, farenList)
+    return render_template('c2f.html', x=zipped)
+
+@app.route('/f2c')
+def f2c():
+    celsiusList = []
+    farenList = []
+    for faren in range(-20,221,10):
+        farenList.append(faren) 
+        celsi = (((faren-32)*5)/9)
+        celsiusList.append(celsi)
+        faren =+ 10
+    zipped = zip(farenList, celsiusList)
+    return render_template('f2c.html', x=zipped)
+	
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name) 
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error404.html'), 404
+
