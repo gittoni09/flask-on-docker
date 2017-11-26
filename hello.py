d93b91b42de8cd11fab13dca5a16748d381be575
gittoni09 <antonio.royo@outlook.com>
2017-11-26 19:47:13 +0100

    Added additional debug info

--- hello.py
+++ hello.py
@@ -12,16 +12,18 @@ app = Flask(__name__)
 #https://stackoverflow.com/questions/12770950/flask-request-remote-addr-is-wrong-on-webfaction-and-not-showing-real-user-ip?noredirect=1&lq=1
 #OK: https://stackoverflow.com/questions/3759981/get-ip-address-of-visitors
    
-#Print host name for debugging purposes
-hostname = os.environ['HOSTNAME']
-print "Hostname: ", hostname
+#Obtain hostname, if it exists 
+try:
+    hostname = os.environ['HOSTNAME']
+except:
+    hostname = "No hostname"
 
 @app.route('/')
 def index():
     currentdatetime = datetime.now()
     clientIP = request.remote_addr
-    print request.environ['REMOTE_ADDR']
-    return render_template('index.html', hostname=hostname, currentdatetime=currentdatetime, clientIP=clientIP )
+    remote_addr = request.environ['REMOTE_ADDR']
+    return render_template('index.html', hostname=hostname, currentdatetime=currentdatetime, clientIP=clientIP, remote_addr=remote_addr )
 
 @app.route('/c2f')
 def c2f():
@@ -34,7 +36,7 @@ def c2f():
         cel =+ 10
     zipped = zip(celsiusList, farenList)
     currentdatetime = datetime.now()
-    return render_template('c2f.html', x=zipped, hostname=hostname, currentdatetime=currentdatetime )
+    return render_template('c2f.html', x=zipped, hostname=hostname, currentdatetime=currentdatetime, clientIP=clientIP, remote_addr=remote_addr )
 
 @app.route('/f2c')
 def f2c():
@@ -47,22 +49,22 @@ def f2c():
         faren =+ 10
     zipped = zip(farenList, celsiusList)
     currentdatetime = datetime.now()
-    return render_template('f2c.html', x=zipped, hostname=hostname, currentdatetime=currentdatetime )
+    return render_template('f2c.html', x=zipped, hostname=hostname, currentdatetime=currentdatetime, clientIP=clientIP, remote_addr=remote_addr)
 
 @app.route('/guesser')
 def guesser():
     currentdatetime = datetime.now()
-    return render_template('guesser.html', hostname=hostname, currentdatetime=currentdatetime )
+    return render_template('guesser.html', hostname=hostname, currentdatetime=currentdatetime, clientIP=clientIP, remote_addr=remote_addr )
 	
 @app.route('/missing')
 def missing():
     currentdatetime = datetime.now()
-    return render_template('missing.html', hostname=hostname, currentdatetime=currentdatetime )
+    return render_template('missing.html', hostname=hostname, currentdatetime=currentdatetime, clientIP=clientIP, remote_addr=remote_addr )
 	
 @app.errorhandler(404)
 def not_found(error):
     currentdatetime = datetime.now()
-    return render_template('error404.html', hostname=hostname, currentdatetime=currentdatetime ), 404
+    return render_template('error404.html', hostname=hostname, currentdatetime=currentdatetime, clientIP=clientIP, remote_addr=remote_addr ), 404
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
